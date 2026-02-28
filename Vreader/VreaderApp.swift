beed5e1a045c1af2b6f7272cec28329f5207f5ae
Vlad P <vp_indigo@mail.ru>
2026-02-28 10:09:54 +0300

    Initial Commit

---
+++ Vreader/VreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VreaderApp.swift
+//  Vreader
+//
+//  Created by P Vlad on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct VreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
